3fc63844b4935359bc7799f2dcbba6010303ff51
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-03-15 17:12:58 +0530

    updating compile file

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+# prepare the source of icons by cloning the repo
+TEMP_DIR=tmp
+mkdir $TEMP_DIR
+SOURCE=$TEMP_DIR/simple-icons
+git clone git@github.com:simple-icons/simple-icons.git $TEMP_DIR/simple-icons
+
 DIRECTORY=$(cd `dirname $0` && pwd)
 RESOURCES=$DIRECTORY/../resources/svg
 
-SOURCE=$1
 echo $SOURCE
 echo "Reading icons..."
 for ICON_DIR in $SOURCE/icons/*; do
@@ -23,4 +28,9 @@ for ICON_DIR in $SOURCE/icons/*; do
 
 done
 
-echo "All done!"
+echo "copied all svgs!"
+echo "Removing "$TEMP_DIR
+rm -rf $TEMP_DIR
+
+echo "All Done!"
+echo "Run `php bin/compile.php` to update the svgs"
