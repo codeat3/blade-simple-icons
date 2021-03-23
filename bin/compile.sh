2cb525e89f1c5e446721b8237ea413eee56a68fc
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-03-23 14:24:15 +0530

    updated README

--- bin/compile.sh
+++ bin/compile.sh
@@ -4,11 +4,15 @@ set -e
 
 # prepare the source of icons by cloning the repo
 TEMP_DIR=blade-icon-temp-dir
+DIRECTORY=$(cd `dirname $0` && pwd)
+
 mkdir -p $TEMP_DIR
 SOURCE=$TEMP_DIR/simple-icons
-git clone git@github.com:simple-icons/simple-icons.git $TEMP_DIR/simple-icons
+# git clone git@github.com:simple-icons/simple-icons.git $TEMP_DIR/simple-icons
+cd $SOURCE
+git pull
+cd $DIRECTORY/../
 
-DIRECTORY=$(cd `dirname $0` && pwd)
 RESOURCES=$DIRECTORY/../resources/svg
 
 echo $SOURCE
@@ -29,8 +33,6 @@ for ICON_DIR in $SOURCE/icons/*; do
 done
 
 echo "copied all svgs!"
-echo "Removing "$TEMP_DIR
-rm -rf $TEMP_DIR
 
 echo "All Done!"
 echo "Run `php bin/compile.php` to update the svgs"
