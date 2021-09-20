650f388336219d645fb23204c8f8068855d236c5
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2021-09-20 09:15:16 +0000

    Fix styling

--- src/BladeSimpleIconsServiceProvider.php
+++ src/BladeSimpleIconsServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Codeat3\BladeSimpleIcons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
 
 final class BladeSimpleIconsServiceProvider extends ServiceProvider
 {
